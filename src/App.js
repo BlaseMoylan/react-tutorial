00876496f7e6499eddc2e53fec0b063ba2107456
Blase Moylan <moylanblase@gmail.com>
2023-03-30 14:07:51 -0700

    Feat: Completed chart tracker componentd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
-
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'},{weight: 175, date: '11-23-2021'}])
@@ -16,6 +16,7 @@ function App() {
         <DisplayEntries parentEntries={entries}/>
         {/* addNewEntryProperty={addNewEntry} this is making a reference not a call it alows addNewEntryProperty to be called off of this reference */}
         <AddEntryForm addNewEntryProperty={addNewEntry}/>
+        <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
